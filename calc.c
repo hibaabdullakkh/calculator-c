e49e8ffe542174afa9717a69ab0c8d65899da387
hiba abdulla <hibaabdullakkh@gmail.com>
2017-02-25 14:35:34 +0530

    added addition

--- calc.c
+++ calc.c
@@ -8,5 +8,17 @@ printf("calculator");
 printf("1.add,2.sub,3.mul,4.div,5.exit");
 printf("enter choice");
 scanf("%d",$c);
+switch(c)
+{
+	case 1: printf("addition");
+	       printf("enter 2 nos");
+		   scanf("%d",&n1);
+	        
+	     scanf("%d",&n2);
+		 sum=n1+n2;
+		 pruntf("sum is %d"sum);
+		 break;
+		 
+}
 }while(c!=5);
 }
